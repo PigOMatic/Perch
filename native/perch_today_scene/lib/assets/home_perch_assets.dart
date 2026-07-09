3436fceb5f9e296706d038ee437d50e7ebd1b0cc
PigOMatic <152893090+PigOMatic@users.noreply.github.com>
2026-07-09 04:10:05 -0400

    Add Home Perch photoreal asset registry

---
+++ native/perch_today_scene/lib/assets/home_perch_assets.dart
@@ -0,0 +1,16 @@
+class HomePerchAssets {
+  const HomePerchAssets._();
+
+  static const sceneRoot = 'assets/scenes/home_perch';
+
+  static const background = '$sceneRoot/background_cabin_desk.webp';
+  static const deskSurface = '$sceneRoot/desk_surface.webp';
+  static const morningLight = '$sceneRoot/morning_light_overlay.webp';
+  static const notebook = '$sceneRoot/notebook_open_blank.webp';
+  static const envelope = '$sceneRoot/envelope_cash_blank.webp';
+  static const sticky = '$sceneRoot/sticky_note_blank.webp';
+  static const shiftTicket = '$sceneRoot/shift_ticket_blank.webp';
+  static const coffeeMug = '$sceneRoot/coffee_mug.webp';
+  static const pen = '$sceneRoot/pen.webp';
+  static const badge = '$sceneRoot/icu_badge.webp';
+}
