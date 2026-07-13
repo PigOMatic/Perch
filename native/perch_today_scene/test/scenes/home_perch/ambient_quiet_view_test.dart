a03b97450d8721e6ba4afcd3b2388d5c8b829f3d
PigOMatic <152893090+PigOMatic@users.noreply.github.com>
2026-07-12 23:26:42 -0400

    Fix Ambient Quiet View keyboard test import

--- native/perch_today_scene/test/scenes/home_perch/ambient_quiet_view_test.dart
+++ native/perch_today_scene/test/scenes/home_perch/ambient_quiet_view_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:perch_today_scene/scenes/home_perch/ambient_quiet_view.dart';
 
