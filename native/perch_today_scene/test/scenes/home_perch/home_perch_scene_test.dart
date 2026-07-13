8a67a30f3cf35827dde6dddd6294a5d2ea5c17a9
PigOMatic <152893090+PigOMatic@users.noreply.github.com>
2026-07-12 22:57:37 -0400

    Use Flutter gestures import in scene tests

--- native/perch_today_scene/test/scenes/home_perch/home_perch_scene_test.dart
+++ native/perch_today_scene/test/scenes/home_perch/home_perch_scene_test.dart
@@ -1,5 +1,4 @@
-import 'dart:ui';
-
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:perch_today_scene/core/brain/perch_brain.dart';
